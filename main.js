322327ccdb0273871a23a3e8a9a5567c2cf743ee
Ronaldo paroski <73145935+RonaldoParoski@users.noreply.github.com>
2023-01-25 17:27:29 -0300

    Alura-Midi

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ function tocaSom(idElementoAudio) { //funcao com parametro para receber o id do 
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); //pega todas as teclas da classe tecla e coloca numa lista
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]; //pega o segundo nome da classe
-    //template string
+                        //template string
     const idAudio = `#som_${instrumento}` //concatena os valores
   
    tecla.onclick = function () { //funcao anonima sem nome pra nao ser executada direto no browser sem interação como usuario
         tocaSom(idAudio)
     }
 
-    contador = contador + 1;
-
-
+    tecla.onkeydown = function() {
+        tecla.classList.add('ativa')
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
 }
